70f732e3322d77295869ce9daebea4a80c0c408a
Freddie Nelson <freddie0208@hotmail.com>
2021-05-24 21:20:47 +0100

    init

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template></template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "App",
+  components: {},
+});
+</script>
+
+<style lang="scss">
+html,
+body {
+  width: 100vw;
+  height: 100vh;
+}
+</style>
