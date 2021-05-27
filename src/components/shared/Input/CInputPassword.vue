d9a7387528aca50ffcf1ea7e2963a3af025ab2d3
Freddie Nelson <freddie0208@hotmail.com>
2021-05-27 16:09:44 +0100

    group in div

--- src/components/shared/Input/CInputPassword.vue
+++ src/components/shared/Input/CInputPassword.vue
@@ -1,18 +1,20 @@
 <template>
-  <c-input-text
-    :name="name"
-    :label="label"
-    :placeholder="placeholder"
-    v-model="value"
-    :censor="censor"
-  />
-  <button
-    class="float-right text-sm text-primary-500 font-medium underline mt-1.5 focus:outline-none transition-colors duration-300 hover:text-accent-500"
-    type="button"
-    @click="censor = !censor"
-  >
-    {{ censor ? "show characters" : "hide characters" }}
-  </button>
+  <div class="flex flex-col">
+    <c-input-text
+      :name="name"
+      :label="label"
+      :placeholder="placeholder"
+      v-model="value"
+      :censor="censor"
+    />
+    <button
+      class="self-end text-sm text-primary-500 font-medium underline mt-1.5 focus:outline-none transition-colors duration-300 hover:text-accent-500 focus:text-accent-500"
+      type="button"
+      @click="censor = !censor"
+    >
+      {{ censor ? "show characters" : "hide characters" }}
+    </button>
+  </div>
 </template>
 
 <script lang="ts">
--- src/components/shared/Input/CInputText.vue
+++ src/components/shared/Input/CInputText.vue
@@ -1,13 +1,15 @@
 <template>
-  <label v-if="label" class="block text-t-sub font-medium" :for="name">{{ label }}</label>
-  <input
-    class="block p-3 border-solid border-2 rounded-lg text-input-blur focus:text-input-focus border-b-light focus:outline-none focus:border-b-highlight transition-colors duration-300"
-    :type="censor ? 'password' : 'text'"
-    :name="name"
-    :placeholder="placeholder"
-    :value="modelValue"
-    @input="$emit('update:modelValue', $event.target.value)"
-  />
+  <div>
+    <label v-if="label" class="block text-t-sub font-medium" :for="name">{{ label }}</label>
+    <input
+      class="block p-3 border-solid border-2 rounded-lg text-input-blur focus:text-input-focus border-b-light focus:outline-none focus:border-b-highlight transition-colors duration-300"
+      :type="censor ? 'password' : 'text'"
+      :name="name"
+      :placeholder="placeholder"
+      :value="modelValue"
+      @input="$emit('update:modelValue', $event.target.value)"
+    />
+  </div>
 </template>
 
 <script lang="ts">
