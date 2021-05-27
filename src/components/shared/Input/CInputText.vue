4b3df89aa8ccd35c703afa94c6357e2b12dbe0e9
Freddie Nelson <freddie0208@hotmail.com>
2021-05-27 15:59:56 +0100

    basic text input

---
+++ src/components/shared/Input/CInputText.vue
@@ -0,0 +1,45 @@
+<template>
+  <label v-if="label" class="block text-t-sub font-medium" :for="name">{{ label }}</label>
+  <input
+    class="block p-3 border-solid border-2 rounded-lg text-input-blur focus:text-input-focus border-b-light focus:outline-none focus:border-b-highlight transition-colors duration-300"
+    :type="censor ? 'password' : 'text'"
+    :name="name"
+    :placeholder="placeholder"
+    :value="modelValue"
+    @input="$emit('update:modelValue', $event.target.value)"
+  />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "CInputText",
+  components: {},
+  props: {
+    modelValue: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    label: {
+      type: String,
+      default: "",
+    },
+    placeholder: {
+      type: String,
+      default: "",
+    },
+    censor: {
+      type: Boolean,
+      default: false,
+    },
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
-const colors = require("tailwindcss/colors")
+const colors = require("tailwindcss/colors");
 
 module.exports = {
   purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
@@ -17,10 +17,15 @@ module.exports = {
       "bg-light": colors.coolGray[50],
       "t-main": colors.coolGray[900],
       "t-sub": colors.coolGray[400],
+      "b-light": colors.coolGray[400],
+      "b-dark": colors.coolGray[600],
+      "b-highlight": colors.purple[400],
+      "input-focus": colors.coolGray[700],
+      "input-blur": colors.coolGray[500],
     },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
