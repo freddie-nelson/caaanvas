65f42b637ad59e7a3c00f3be74738be790aa7796
Freddie Nelson <freddie0208@hotmail.com>
2021-05-30 20:32:21 +0100

    make canvas toolbar seperate component

---
+++ src/components/app/CCanvasToolbar.vue
@@ -0,0 +1,49 @@
+<template>
+  <aside
+    class="absolute left-4 top-0 bottom-0 my-auto text-bg-light flex flex-col w-12 bg-bg-dark rounded-md overflow-hidden"
+    style="height: min-content"
+  >
+    <button
+      v-for="tool in tools"
+      :key="tool.name"
+      class="w-full h-12 my-2 bg-bg-dark flex justify-center items-center opacity-40 hover:opacity-90 transition-all duration-300 outline-none focus:outline-none"
+      :class="
+        selectedTool === tool.name
+          ? 'text-accent-500 opacity-90'
+          : 'focus:text-accent-300 focus:opacity-90'
+      "
+      @click="selectedTool = tool.name"
+    >
+      <Icon class="w-6 h-6" :icon="tool.icon" />
+    </button>
+  </aside>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+
+import { Icon } from "@iconify/vue";
+
+export default defineComponent({
+  name: "CCanvasToolbar",
+  components: {
+    Icon,
+  },
+  props: {
+    tools: {
+      type: Array as () => { name: string; icon: string }[],
+      required: true,
+    },
+  },
+  setup(props) {
+    const selectedTool = ref(props.tools[0].name);
+
+    return {
+      selectedTool,
+    };
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+</style>
--- src/views/Canvas.vue
+++ src/views/Canvas.vue
@@ -3,29 +3,14 @@
   <main class="w-full h-full bg-bg-light"></main>
 
   <!-- toolbar -->
-  <aside
-    class="absolute left-4 top-0 bottom-0 my-auto text-bg-light flex flex-col w-12 bg-bg-dark rounded-md overflow-hidden"
-    style="height: min-content"
-  >
-    <button
-      v-for="tool in tools"
-      :key="tool.name"
-      class="w-full h-12 my-2 bg-bg-dark flex justify-center items-center opacity-40 hover:opacity-90 transition-all duration-300 outline-none focus:outline-none"
-      :class="
-        selectedTool === tool.name
-          ? 'text-accent-500 opacity-90'
-          : 'focus:text-accent-300 focus:opacity-90'
-      "
-    >
-      <Icon class="w-6 h-6" :icon="tool.icon" />
-    </button>
-  </aside>
+  <c-canvas-toolbar :tools="tools" />
 </template>
 
 <script lang="ts">
 import { defineComponent, reactive, ref } from "vue";
 
-import { Icon } from "@iconify/vue";
+import CCanvasToolbar from "@/components/app/CCanvasToolbar.vue";
+
 import textIcon from "@iconify-icons/feather/type";
 import linkIcon from "@iconify-icons/feather/link";
 import imageIcon from "@iconify-icons/feather/image";
@@ -35,7 +20,7 @@ import checkpointIcon from "@iconify-icons/feather/flag";
 export default defineComponent({
   name: "Canvas",
   components: {
-    Icon,
+    CCanvasToolbar,
   },
   setup() {
     const selectedTool = ref("text");
