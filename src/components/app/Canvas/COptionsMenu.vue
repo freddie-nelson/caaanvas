5e8cde7d57c0cce3d3852b69d8c7df36b4f882a9
Freddie Nelson <freddie0208@hotmail.com>
2021-06-14 20:41:13 +0100

    move options menu to seperate component

---
+++ src/components/app/Canvas/COptionsMenu.vue
@@ -0,0 +1,83 @@
+<template>
+  <div
+    v-show="isOptionsVisible"
+    ref="options"
+    class="absolute top-0 left-0 w-52 h-12 bg-bg-dark rounded-md"
+    :style="{
+      transform: `scale(${$store.state.canvas.zoom.scale}) translate(${optionsPos.x}px, ${optionsPos.y}px)`,
+    }"
+  ></div>
+</template>
+
+<script lang="ts">
+import useComponentEvent from "@/utils/useComponentEvent";
+import { defineComponent, ref, reactive, computed, watch } from "vue";
+
+import { Component } from "@/store";
+
+interface eventObj {
+  target: Element;
+  c: Component;
+}
+
+export default defineComponent({
+  name: "COptionsMenu",
+  components: {},
+  props: {
+    show: {
+      type: Boolean,
+      required: true,
+    },
+    info: {
+      type: Object as () => eventObj,
+    },
+  },
+  setup(props) {
+    const options = ref(document.createElement("div"));
+    const isOptionsVisible = ref(false);
+    let optionsTarget: Element;
+    const optionsPos = reactive({
+      x: 0,
+      y: 0,
+    });
+
+    const showOptions = (e: eventObj) => {
+      isOptionsVisible.value = true;
+      optionsTarget = e.target;
+
+      const box = e.target.getBoundingClientRect();
+      optionsPos.x = box.x;
+      optionsPos.y = box.y + box.height + 7;
+    };
+
+    watch(
+      computed(() => props.show),
+      () => {
+        if (props.info) showOptions(props.info);
+      },
+    );
+
+    useComponentEvent(document.body, "click", (event) => {
+      if (!optionsTarget) return;
+      const e = <MouseEvent>event;
+
+      const containsTarget =
+        optionsTarget.compareDocumentPosition(e.target as Node) &
+        Node.DOCUMENT_POSITION_CONTAINED_BY;
+
+      if (e.target !== optionsTarget && e.target !== options.value && !containsTarget)
+        isOptionsVisible.value = false;
+    });
+
+    return {
+      options,
+      isOptionsVisible,
+      optionsPos,
+      showOptions,
+    };
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+</style>
