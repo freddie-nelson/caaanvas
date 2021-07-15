c8885272461625ed118db2c3f933faef38c2447a
Freddie Nelson <freddie0208@hotmail.com>
2021-07-15 23:22:46 +0100

    add button dropdown

--- src/components/app/Canvas/CZoomControls.vue
+++ src/components/app/Canvas/CZoomControls.vue
@@ -1,38 +1,20 @@
 <template>
-  <div
-    ref="container"
-    class="
-      absolute
-      top-5
-      right-5
-      rounded-md
-      bg-bg-dark
-      font-mono
-      flex
-      items-center
-      p-4
-      font-medium
-      text-sm text-bg-light
-      z-10
-    "
+  <c-button-dropdown
+    :show="showZoomMenu"
+    @updateShow="showZoomMenu = $event"
+    :forceClose="forceCloseMenu"
+    tag="zoom-dropdown"
+    class="font-mono flex items-center py-4 pl-4 font-medium text-sm text-bg-light"
   >
-    <button zoom-dropdown class="flex items-center focus:outline-none" @click="toggleZoomMenu">
-      <p class="pointer-events-none">{{ $store.state.canvas.zoom.level }}%</p>
+    <p class="pointer-events-none">{{ $store.state.canvas.zoom.level }}%</p>
 
-      <Icon
-        class="pointer-events-none w-4 h-4 ml-1 transform transition-transform focus:rotate-180"
-        :class="{ 'rotate-180': showZoomMenu }"
-        :icon="icons.chevronDown"
-      />
-    </button>
+    <Icon
+      class="pointer-events-none w-4 h-4 ml-1 transform transition-transform focus:rotate-180"
+      :class="{ 'rotate-180': showZoomMenu }"
+      :icon="icons.chevronDown"
+    />
 
-    <div
-      v-show="showZoomMenu"
-      zoom-dropdown
-      ui-element
-      :style="{ right: `${zoomMenuRight + 3}px`, top: `${zoomMenuTop + 3}px` }"
-      class="absolute bg-t-main px-4 py-2.5 rounded-md"
-    >
+    <template v-slot:menu>
       <button
         v-for="i in 10"
         :key="i"
@@ -46,18 +28,19 @@
         "
         @click="
           $store.commit('SET_ZOOM_SCALE', i / 10);
-          toggleZoomMenu($event);
+          forceCloseMenu = !forceCloseMenu;
         "
       >
         {{ i * 10 }}%
       </button>
-    </div>
-  </div>
+    </template>
+  </c-button-dropdown>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref } from "vue";
-import useComponentEvent from "@/utils/useComponentEvent";
+
+import CButtonDropdown from "@/components/shared/Button/CButtonDropdown.vue";
 
 import { Icon } from "@iconify/vue";
 import chevronDownIcon from "@iconify-icons/feather/chevron-down";
@@ -65,41 +48,16 @@ import chevronDownIcon from "@iconify-icons/feather/chevron-down";
 export default defineComponent({
   name: "CZoomControls",
   components: {
+    CButtonDropdown,
     Icon,
   },
   setup() {
-    const container = ref(document.createElement("div"));
-
     const showZoomMenu = ref(false);
-    const zoomMenuRight = ref(0);
-    const zoomMenuTop = ref(0);
-
-    const toggleZoomMenu = (e: MouseEvent) => {
-      showZoomMenu.value = !showZoomMenu.value;
-
-      const box = container.value.getBoundingClientRect();
-
-      const mouseX = e.x || box.left + box.width;
-      const mouseY = e.y || box.top + box.height / 2;
-
-      const right = window.innerWidth - mouseX - (window.innerWidth - box.right);
-      const top = mouseY - box.top;
-
-      zoomMenuRight.value = right;
-      zoomMenuTop.value = top;
-    };
-
-    useComponentEvent(document.body, "click", (e) => {
-      if (!(e.target as Element).hasAttribute("zoom-dropdown")) showZoomMenu.value = false;
-    });
+    const forceCloseMenu = ref(false);
 
     return {
-      container,
-
       showZoomMenu,
-      zoomMenuRight,
-      zoomMenuTop,
-      toggleZoomMenu,
+      forceCloseMenu,
 
       icons: {
         chevronDown: chevronDownIcon,
---
+++ src/components/shared/Button/CButtonDropdown.vue
@@ -0,0 +1,87 @@
+<template>
+  <div ref="container" class="relative">
+    <button v-bind="attribute" class="flex items-center focus:outline-none" @click="toggleMenu">
+      <slot></slot>
+    </button>
+
+    <div
+      v-show="showMenu"
+      v-bind="attribute"
+      ui-element
+      :style="{ right: `${menuRight + 3}px`, top: `${menuTop + 3}px` }"
+      class="absolute bg-t-main px-4 py-2.5 rounded-md"
+    >
+      <slot name="menu"></slot>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { computed, defineComponent, ref, watch } from "vue";
+import useComponentEvent from "@/utils/useComponentEvent";
+
+export default defineComponent({
+  name: "CButtonDropdown",
+  components: {},
+  props: {
+    tag: {
+      type: String,
+      required: true,
+    },
+    forceClose: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  setup(props, { emit }) {
+    const container = ref(document.createElement("div"));
+
+    const showMenu = ref(false);
+    watch(showMenu, () => emit("updateShow", showMenu.value));
+    watch(
+      computed(() => props.forceClose),
+      () => (showMenu.value = false),
+    );
+
+    const menuRight = ref(0);
+    const menuTop = ref(0);
+
+    const toggleMenu = (e: MouseEvent) => {
+      showMenu.value = !showMenu.value;
+
+      const box = container.value.getBoundingClientRect();
+
+      const mouseX = e.x || box.left + box.width;
+      const mouseY = e.y || box.top + box.height / 2;
+
+      const right = window.innerWidth - mouseX - (window.innerWidth - box.right);
+      const top = mouseY - box.top;
+
+      menuRight.value = right;
+      menuTop.value = top;
+    };
+
+    useComponentEvent(document.body, "click", (e) => {
+      if (!(e.target as Element).hasAttribute(props.tag)) showMenu.value = false;
+    });
+
+    const attribute = {
+      [props.tag]: "",
+    };
+
+    return {
+      container,
+
+      showMenu,
+      menuRight,
+      menuTop,
+      toggleMenu,
+
+      attribute,
+    };
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+</style>
