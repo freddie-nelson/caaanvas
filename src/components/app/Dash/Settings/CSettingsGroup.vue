028c6c6941213a142a23dc894e09e04cdd9df25a
Freddie <freddie0208@hotmail.com>
2021-08-04 23:23:32 +0100

    create settings helper components

---
+++ src/components/app/Dash/Settings/CSettingsGroup.vue
@@ -0,0 +1,25 @@
+<template>
+  <section class="mt-10 pl-10">
+    <h2 class="text-4xl font-bold">{{ title }}</h2>
+
+    <slot></slot>
+  </section>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "CSettingsGroup",
+  components: {},
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ src/components/app/Dash/Settings/CSettingsSetting.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="mt-8 pl-7">
+    <h3 class="text-2xl font-bold mb-4">{{ name }}</h3>
+
+    <div class="pl-4">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "CSettingsSetting",
+  components: {},
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+</style>
