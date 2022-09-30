1569c55d43c7777e5b7d10e102cfa16ee6a17af5
Freddie Nelson <47699795+freddie-nelson@users.noreply.github.com>
2022-09-30 17:19:33 +0100

    add firebaseConfig

---
+++ web/firebaseConfig.ts
@@ -0,0 +1,9 @@
+export default {
+  apiKey: "AIzaSyBrXWVVkmSADKSmB2KVvRpUW2FPQ5pWbgI",
+  authDomain: "caaanvas.firebaseapp.com",
+  projectId: "caaanvas",
+  storageBucket: "caaanvas.appspot.com",
+  messagingSenderId: "907874398688",
+  appId: "1:907874398688:web:feaea0810a50a887c2fb85",
+  measurementId: "G-B0QGZPVWLD",
+};
