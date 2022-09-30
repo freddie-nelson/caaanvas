5bc3aee5eaf0bc59fa03071ade8ff8c69aa94fe6
Freddie Nelson <47699795+freddie-nelson@users.noreply.github.com>
2022-09-30 17:24:14 +0100

    force tw to compile all text sizes

--- web/src/App.vue
+++ web/src/App.vue
@@ -2,6 +2,25 @@
   <c-toast-controller />
   <router-view></router-view>
   <div id="modals"></div>
+
+  <!-- force tailwind compile all text sizes -->
+  <div
+    class="
+      text-xs
+      text-sm
+      text-base
+      text-lg
+      text-xl
+      text-2xl
+      text-3xl
+      text-4xl
+      text-5xl
+      text-6xl
+      text-7xl
+      text-8xl
+      text-9xl
+    "
+  ></div>
 </template>
 
 <script lang="ts">
